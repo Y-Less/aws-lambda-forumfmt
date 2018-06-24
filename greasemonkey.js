5ff62a7606350bda8953a4e104a5546f4d90b8b6
Y_Less <git@y-less.com>
2018-06-24 21:43:27 +0200

    Tab actions.

--- greasemonkey.js
+++ greasemonkey.js
@@ -46,10 +46,74 @@
   }
 
   function doTab(textbox, shift) {
-    if (shift) {
-      console.log("back");
+    var start = textbox.selectionStart; // Mark the bounds of the selection.
+    var end = textbox.selectionEnd;     // Mark the bounds of the selection.
+    var text = textbox.value;
+    var nl = text.indexOf('\n', start);
+    if (nl >= end || nl === -1) {
+      if (shift) {
+        // Unindent, or go to the start of the line.
+        var prev = text.lastIndexOf('\n', start) + 1;
+        if (text[prev] === '\t') {
+          textbox.value = text.substring(0, prev) + text.substring(prev + 1);
+          textbox.selectionStart = textbox.selectionEnd = Math.max(prev, start - 1);
+        }
+      } else {
+        // Point or single line:  Just add a tab here.
+        textbox.value = text.substring(0, start) + '\t' + text.substring(end);
+        textbox.selectionStart = textbox.selectionEnd = start + 1;
+      }
     } else {
-      console.log("fore");
+      // Multi-line:  Indent.
+      // First, collect the lines to indent.
+      var prev = text.lastIndexOf('\n', start) + 1;
+      var next;
+      var begin = prev;
+      var finish; // Mark the bounds of the affected lines.
+      var lines = [];
+      // Put the affected lines in an array.
+      for ( ; ; ) {
+        next = text.indexOf('\n', prev);
+        if (next === -1) {
+          lines.push(text.substring(prev));
+          finish = text.length;
+          break;
+        } else {
+          lines.push(text.substring(prev, next));
+          if (next >= end) {
+            finish = next;
+            break;
+          } else {
+            prev = next + 1;
+          }
+        }
+      }
+      // Debug print the lines.
+      //console.log(lines);
+      if (shift) {
+        var reduced = 0;
+        // Backwards - harder!
+        textbox.value = text.substring(0, begin) + lines.map(function (x) {
+          console.log("0: " + x[0]);
+          console.log(x.substring(1));
+          if (x[0] === '\t') {
+            ++reduced;
+            return x.substring(1);
+          } else {
+            return x;
+          }
+        }).join('\n') + text.substring(finish);
+        // Reselect from `start - 1` to `end - number of lines` (depending on how many lines were
+        // actually changed).
+        textbox.selectionStart = (lines[0][0] === '\t') ? Math.max(begin, start - 1) : start;
+        textbox.selectionEnd = end - reduced + ((text[end - 1] === '\n' && text[end] === '\t') ? 1 : 0);
+      } else {
+        // Forwards - easy!
+        textbox.value = text.substring(0, begin) + lines.map(function (x) { return '\t' + x; }).join('\n') + text.substring(finish);
+        // Reselect from `start + 1` to `end + number of lines`.
+        textbox.selectionStart = start + 1;
+        textbox.selectionEnd = end + lines.length;
+      }
     }
   }
 
@@ -65,7 +129,7 @@
           event.preventDefault();
         } else if (event.keyCode === 13 && !event.shiftKey && event.ctrlKey && !event.altKey && !event.metaKey) {
           // ctrl-enter.
-          if (form.onsubmit({})) {
+          if (form.onsubmit.call(form, {})) { // Must use ".call", ignore Tampermonkey!
             // The default submission handler didn't prevent us submitting.
             replaceTextAndSubmit(form, textbox);
           }
