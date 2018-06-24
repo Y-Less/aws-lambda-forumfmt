704bf8fe5e3199fbf671d53f7a6bb55701059315
Y_Less <git@y-less.com>
2018-06-24 13:47:15 +0200

    Make it work on AWS.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"log"
 	"errors"
 	"fmt"
 	"strings"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/lambda"
 	"./forumfmt/markdown"
 	"github.com/Jeffail/gabs"
@@ -18,22 +20,57 @@ type MyResponse struct {
 	BB string `json:"BB"`
 }
 
-func LambdaHandler(event MyEvent) (MyResponse, error) {
+type MyHeaders struct {
+}
+
+type AWSResponse struct {
+	IsBase64Encoded bool `json:"isBase64Encoded"`
+	StatusCode int `json:"statusCode"`
+	Headers MyHeaders `json:"headers"`
+	Body string `json:"body"`
+}
+
+type AWSRequest struct {
+	Body string `json:"body"`
+}
+
+func AWSError(err error) (AWSResponse, error) {
+	return AWSResponse{
+		IsBase64Encoded: false,
+		StatusCode: 500,
+		Headers: MyHeaders{},
+		Body: err.Error() }, err
+}
+
+func LambdaHandler(awsevent AWSRequest) (AWSResponse, error) {
+	event := new(MyEvent)
+	_ = json.Unmarshal([]byte(awsevent.Body), &event)
+	log.Print("Hello from Lambda")
 	var (
 		out strings.Builder
 		err        error
 		jsonParsed *gabs.Container
 	)
+	log.Print(event.MD)
 	jsonParsed, err = markdown.ParseStyles(event.Style)
 	if err != nil {
-		return MyResponse{}, errors.New(fmt.Sprintf("failed to process styles:", err))
+		return AWSError(errors.New(fmt.Sprintf("failed to process styles:", err)))
 	}
 
 	err = markdown.Process(strings.NewReader(event.MD), &out, jsonParsed)
 	if err != nil {
-		return  MyResponse{}, errors.New(fmt.Sprintf("failed to process input:", err))
+		return  AWSError(errors.New(fmt.Sprintf("failed to process input:", err)))
 	}
-	return MyResponse{BB: out.String()}, nil
+	body, err := json.Marshal(MyResponse{BB: out.String()})
+	log.Print(out.String())
+	if err != nil {
+		return AWSError(errors.New(fmt.Sprintf("failed to marshal response:", err)))
+	}
+	return AWSResponse{
+		IsBase64Encoded: false,
+		StatusCode: 200,
+		Headers: MyHeaders{},
+		Body: string(body) }, nil
 }
 
 func main() {
