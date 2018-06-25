42cc888aa780c4f894487579ea5e80d45b4ce51c
Y_Less <git@y-less.com>
2018-06-25 16:28:54 +0200

    Direct link...

--- samp-forum-enhancer.user.js
+++ samp-forum-enhancer.user.js
@@ -3,6 +3,7 @@
 // @namespace    https://y-less.com/
 // @version      0.1
 // @copyright    2018, Alex "Y_Less" Cole
+// @license      https://www.mozilla.org/en-US/MPL/1.1/
 // @description  Enhance the SA:MP forums.
 // @author       Y_Less
 // @match        http://forum.sa-mp.com/*
